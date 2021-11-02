74ac728462a5ce37deb66eb2b50ff1bb43279013
Nhung-Ubuntu-Home <nnhung2@gmail.com>
2021-11-02 09:59:32 +0100

    Ex05 cpp01 ok

---
+++ cpp01b/ex05/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Karen.hpp"
+
+int     main(void)
+{
+    Karen karen;
+
+    karen.complain("DEBUG");
+    karen.complain("INFO");
+    karen.complain("WARNING");
+    karen.complain("ERROR");
+    karen.complain("lkjelj");
+    
+
+    return (0);
+}
